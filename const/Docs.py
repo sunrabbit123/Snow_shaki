9d4246f606813b76a6a24b31b2b0331c7a9dd939
sunrabbit123 <qudwls185@naver.com>
2020-12-29 21:21:26 +0900

    resort

--- const/Docs.py
+++ const/Docs.py
@@ -1,33 +1,35 @@
 class Docs:
-    help =\
-        '''
-        **샤키야 굴러**
+    help_title = ["샤키야 도움말", "샤키야 굴러", "샤키야 검색", "샤키야 커맨드", "샤키야 골라", "샤키야 급식"]
+    help_description = [
+        """
+        `샤키의 명령어 목록들을 보여줍니다..!`
+        """,
+        """
         `사실 이 봇의 핵심이자 제일 중요한 기능`
         `무려 샤키가 굴러줍니다`
-
-        **샤키야 검색**
+        """,
+        """
         `샤키야 구글검색 (키워드)`
         `샤키야 사전검색 (키워드)`
         샤키가 직접 검색을 해줍니다.
-
-        **샤키야 커맨드**
+        """,
+        """
         `샤키야 배워 (key command) : (value command)`
         `샤키야 잊어 (key command)`
         `샤키야 (key command)`
         이 세가지의 명령어로 샤키 커맨드를 이용할 수 있습니다.
-
-        **샤키야 골라**
+        """,
+        """
         `샤키야 골라 (args1) (args2) (args3)`
         args들 중 하나를 랜덤으로 고릅니다.
         띄어쓰기로 구분되니 주의해주세요.
-
-        **샤키야 급식**
+        """,
+        """
         `샤키야 급식`
         해당 명령어는 오늘의 급식들을 보여줍니다.
         `샤키야 급식 내일`
         해당 명령어는 내일의 급식들을 보여줍니다.
-
-        **※ 후원은 -> 농협 356-1336-2154-93  <- 여기로 ※**
-        '''
-    url = 'https://discord.com/api/oauth2/authorize?client_id=700605291196186634&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D700605291196186634%26permissions%3D8%26redirect_uri%3Dhttps%253A%252F%252Fdiscord.com%252Fapi%252Foauth2%252Fauthorize%253Fclient_id%253D700605&scope=bot'
-        
+        """,
+    ]
+    url = "https://discord.com/api/oauth2/authorize?client_id=700605291196186634&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D700605291196186634%26permissions%3D8%26redirect_uri%3Dhttps%253A%252F%252Fdiscord.com%252Fapi%252Foauth2%252Fauthorize%253Fclient_id%253D700605&scope=bot"
+    NH = "※ 후원은 -> 농협 356-1336-2154-93  <- 여기로 ※"
--- model/command.py
+++ model/command.py
@@ -2,33 +2,34 @@ import pymongo
 from random import choice
 import asyncio
 
+
 class custom_command:
     def __init__(self, db):
         # db : mongodb database
         self.collect = db
-    
-    def command_insert(self, key, value, server, user) -> bool :
-        self.collect.insert({"key-command" : key,
-                            "value-command" : value,
-                            "server" : server,
-                            "user" : user})
+
+    def command_insert(self, key, value, server, user) -> bool:
+        self.collect.insert(
+            {"key-command": key, "value-command": value, "server": server, "user": user}
+        )
         return True
-    
+
     def command_select(self, message) -> str:
-        key = ' '.join(message.content.split()[1:])
-        result = list(self.collect.find({
-            "key-command" : key
-        },{
-            "_id" : False,
-            "key-command" : True,
-            "value-command" : True,
-            "server" : True,
-            "user" : True
-        }))
+        key = " ".join(message.content.split()[1:])
+        result = list(
+            self.collect.find(
+                {"key-command": key},
+                {
+                    "_id": False,
+                    "key-command": True,
+                    "value-command": True,
+                    "server": True,
+                    "user": True,
+                },
+            )
+        )
         return result
 
-    def command_delete(self, key) -> bool :
-        self.collect.remove({
-            "key-command" : key
-        })
+    def command_delete(self, key) -> bool:
+        self.collect.remove({"key-command": key})
         return True
