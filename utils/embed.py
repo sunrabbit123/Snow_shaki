70889cebb05fd054175df01ca4381a0f171e6f7e
sunrabbit123 <qudwls185@naver.com>
2021-07-08 23:40:32 +0900

    add: not_found_school

--- utils/embed.py
+++ utils/embed.py
@@ -7,3 +7,9 @@ def set_embed(ext: discord.Message, title: str = None, description: str = None):
     )
     embed.set_footer(text=ext.author.name, icon_url=ext.author.avatar_url)
     return embed
+
+
+def not_found_school(ext: discord.Message):
+    return set_embed(
+        ext, description="야발,, 학교도 등록이 안되어있는데 뭘ㄹ,,\n**샤키야 학교검색 학교이름**이라고 해보던가,,"
+    )
