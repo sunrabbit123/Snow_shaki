f912e124e1fe807e3b63de6f0dffd66a28d948de
sunrabbit123 <qudwls185@naver.com>
2021-07-08 23:41:51 +0900

    update: import 방법

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
-from .decorator import print_time
-from .embed import set_embed
-from .date_converter import get_date
-from .web_find import SearchWord, NeisAPI
-from .string import StringManger
+from .decorator import *
+from .embed import *
+from .date_converter import *
+from .web_find import *
+from .string import *
