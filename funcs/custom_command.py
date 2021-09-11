35f69ae02d1dcbedfa31833cbfae8ebbfebf618f
sunrabbit123 <qudwls185@naver.com>
2021-09-11 23:18:24 +0900

    fix : db 인식 실패 문제 해결

--- funcs/custom_command.py
+++ funcs/custom_command.py
@@ -1,11 +1,12 @@
 import discord
 
+from model import CustomCommandModel as CCM
 
 class CustomCommand:
     @staticmethod
     async def command_잊어(message: discord.Message, db):  # 샤키야 key커맨드
         key = message.content.split()[2]
-        result = db.command_delete(key)
+        result = CCM(db).command_delete(key)
 
         if result:
             await message.channel.send("그게,,, 뭐죠,,,?")
@@ -13,7 +14,7 @@ class CustomCommand:
     @staticmethod
     async def command_배워(message: discord.Message, db):  # 샤키야 배워 key커맨드 value커맨드
         word = " ".join(message.content.split()[2:]).split(":")
-        db.command_insert(word[0], word[1], message.channel.name, message.author.name)
+        CCM(db).command_insert(word[0], word[1], message.channel.name, message.author.name)
         # key, value, server, user
 
         await message.channel.send("야랄 왜 나한테...")
