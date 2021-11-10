a4bfccb375b4903aedbadc6aabaead912e7bd445
sunrabbit123 <qudwls185@naver.com>
2021-11-10 17:36:58 +0900

    add : key_command, value_command 구분자 업데이트

--- funcs/custom_command.py
+++ funcs/custom_command.py
@@ -14,7 +14,9 @@ class CustomCommand:
     @staticmethod
     async def command_배워(message: discord.Message, db):  # 샤키야 배워 key커맨드 value커맨드
         word = " ".join(message.content.split()[2:]).split(":")
-        CCM(db).command_insert(word[0], word[1], message.channel.name, message.author.name)
+        key_command = word[0]
+        value_command = ":".join(word[1:])
+        CCM(db).command_insert(key_command, value_command, message.channel.name, message.author.name)
         # key, value, server, user
 
         await message.channel.send("야랄 왜 나한테...")
